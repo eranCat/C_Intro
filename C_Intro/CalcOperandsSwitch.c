6fd10199c266a1b727c3d0e0d1a707a1190cac80
Karaso Eran <karasoe@my.hit.ac.il>
2022-10-04 22:52:17 +0300

    Calculator from console

---
+++ C_Intro/CalcOperandsSwitch.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+
+void main() {
+	double x, y;
+	char op;
+
+	printf("enter two operands\n");
+	scanf_s("%lf%lf", &x, &y);
+
+	//flushall();//clear console
+	//_flushall();//clear console
+
+	printf("enter operator\n");
+	scanf_s(" %c", &op);// op=getchar();
+
+	//for scanning a char, there's an issue with "%c" , so we need to put a space like so " %c"
+	//https://stackoverflow.com/questions/13542055/how-to-do-scanf-for-single-char-in-c
+
+	switch (op)
+	{
+	case '+': printf("Result is % lf\n", x + y);
+		break;
+	case '-': printf("Result is % lf\n", x - y);
+		break;
+	case '*': printf("Result is % lf\n", x * y);
+		break;
+	case '/': if (fabs(y) >= 0.00000001) printf("Result is % lf\n", x / y);
+			  else printf("Error\n");
+		break;
+	default: printf("Invalid operator\n");
+	}
+}
--- C_Intro/ConvertCharGrade.c
+++ C_Intro/ConvertCharGrade.c
@@ -1,24 +1,24 @@
 #include<stdio.h>
 
-void main() {
-	char grade;
-	printf("Enter a grade - A\B\C \n");
-	grade = getchar(); // the function (from stdio) reads one character and stores it in grade
-	//grade = scanf_s(“%c”,&grade);
-	switch (grade)
-	{
-	case 'A':
-		printf("90 - 100\n");
-		break;
-	case 'B':
-		printf("80 - 89\n");
-		break;
-	case 'C':
-		printf("70 - 79\n");
-		break;
-
-	default:
-		printf("Error!\n");
-
-	}
-}
+//void main() {
+//	char grade;
+//	printf("Enter a grade - A\B\C \n");
+//	grade = getchar(); // the function (from stdio) reads one character and stores it in grade
+//	//grade = scanf_s(“%c”,&grade);
+//	switch (grade)
+//	{
+//	case 'A':
+//		printf("90 - 100\n");
+//		break;
+//	case 'B':
+//		printf("80 - 89\n");
+//		break;
+//	case 'C':
+//		printf("70 - 79\n");
+//		break;
+//
+//	default:
+//		printf("Error!\n");
+//
+//	}
+//}
