d276ebbbe1dddf5ba84357fe7019e7cd7fb15b9a
Karaso Eran <karasoe@my.hit.ac.il>
2022-10-04 21:57:50 +0300

    Print max of 2 numbers

--- C_Intro/If_Even.c
+++ C_Intro/If_Even.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 
-void main() {
-	int n;
-	printf("Choose an integer\n");
-	scanf_s("%d", &n);
-	if (n == 0)
-	{
-		printf("%d is neither even or odd.\n", n);
-	}else	if (n%2==0)
-	{
-		printf("%d is even.\n",n);
-	}
-	else {
-		printf("%d is odd.\n",n);
-	}
-}
+//void main() {
+//	int n;
+//	printf("Choose an integer\n");
+//	scanf_s("%d", &n);
+//	if (n == 0)
+//	{
+//		printf("%d is neither even or odd.\n", n);
+//	}else	if (n%2==0)
+//	{
+//		printf("%d is even.\n",n);
+//	}
+//	else {
+//		printf("%d is odd.\n",n);
+//	}
+//}
