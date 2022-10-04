5e3211444237483db0577274ba0f657b10e3f20c
Karaso Eran <karasoe@my.hit.ac.il>
2022-10-04 20:10:14 +0300

    sum of 2 numbers from input

---
+++ C_Intro/ConvertToKM.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+//void main() {
+//	int miles, yards;
+//	double kilometers;
+//	miles = 26;
+//	yards = 385;
+//	
+//	kilometers = 1.609 * (miles + yards / 1760.0);
+//	//printf("%lf KM\n", kilometers);//lf - a large float - double number
+//	printf("%.3lf KM\n", kilometers);//.3lf - 5 digits after decimal point
+//}
---
+++ C_Intro/Sum2Inputs.c
@@ -0,0 +1,17 @@
+/*Write a program that gets 2 integers in vars a,b .
+sets a var named sum with their sum , and prints sum.*/
+#include<stdio.h>
+
+//scanf is not working in VS , so we use scanf_s instead - teachnically C can work with scanf
+//void main() {
+//	int a, b;
+//
+//	printf("First number : \n");
+//	scanf_s("%d", &a);
+//
+//	printf("Second number : \n");
+//	scanf_s("%d", &b);
+//
+//	long sum = a + b;
+//	printf("The sum of %d and %d is %ld\n", a, b, sum);
+//}
