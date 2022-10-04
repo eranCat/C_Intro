4ab8b96e0ddd28bac41e535427a38c9671829cd1
Karaso Eran <karasoe@my.hit.ac.il>
2022-10-04 22:14:58 +0300

    Check leap year

---
+++ C_Intro/LeapYearCheck.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*Example 
+* Any year that is evenly divisible by 4 is a leap year
+	2000 leap
+	1992 leap
+	1900 not leap
+*/
+void main()
+{
+	int year;
+	printf("Enter a year for checking:");
+	scanf_s("%d", &year);
+	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+		printf("This year is leap\n");
+	else
+		printf("This year is not leap\n");
+}
