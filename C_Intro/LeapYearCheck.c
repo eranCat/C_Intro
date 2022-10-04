84c75c60b951e23a28769795b1ecac713a132535
Karaso Eran <karasoe@my.hit.ac.il>
2022-10-04 22:30:25 +0300

    convert char grade

--- C_Intro/LeapYearCheck.c
+++ C_Intro/LeapYearCheck.c
@@ -5,13 +5,13 @@
 	1992 leap
 	1900 not leap
 */
-void main()
-{
-	int year;
-	printf("Enter a year for checking:");
-	scanf_s("%d", &year);
-	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
-		printf("This year is leap\n");
-	else
-		printf("This year is not leap\n");
-}
+//void main()
+//{
+//	int year;
+//	printf("Enter a year for checking:");
+//	scanf_s("%d", &year);
+//	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+//		printf("This year is leap\n");
+//	else
+//		printf("This year is not leap\n");
+//}
