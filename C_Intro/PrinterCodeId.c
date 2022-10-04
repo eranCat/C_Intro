79dac9ac73c3602d6911ec09d99abc7983a32d77
Karaso Eran <karasoe@my.hit.ac.il>
2022-10-04 20:28:24 +0300

    print id sum code

---
+++ C_Intro/PrinterCodeId.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/*Program that calculates the sum of 2 last digits, and the first digits of an id
+Example : for id 123456789, needs to be 12 + 89 = 101*/
+void main() {
+	long id , first, last;
+
+	printf("ID:\n");
+	scanf_s("%ld", &id);
+
+	last = id % 100;//10^2 // 2 first digits
+	first = id / 10000000;//assuming the id is 9 digits
+
+	printf("first %d\nlast %d\n", first, last);
+
+	int sum = last + first;
+	printf("Code for printer: %d\n", sum);
+}
--- C_Intro/Sum2Inputs.c
+++ C_Intro/Sum2Inputs.c
@@ -3,18 +3,18 @@ sets a var named sum with their sum , and prints sum.*/
 #include<stdio.h>
 
 //scanf is not working in VS , so we use scanf_s instead - teachnically C can work with scanf
-void main() {
-	int a=0, b=0;
-
-	/*printf("First number : \n");
-	scanf_s("%d", &a);
-
-	printf("Second number : \n");
-	scanf_s("%d", &b);*/
-
-	printf("Enter 2 integers:\n");
-	scanf_s("%d%d", &a, &b);
-
-	long sum = a + b;
-	printf("The sum of %d and %d is %ld\n", a, b, sum);
-}
+//void main() {
+//	int a=0, b=0;
+//
+//	/*printf("First number : \n");
+//	scanf_s("%d", &a);
+//
+//	printf("Second number : \n");
+//	scanf_s("%d", &b);*/
+//
+//	printf("Enter 2 integers:\n");
+//	scanf_s("%d%d", &a, &b);
+//
+//	long sum = a + b;
+//	printf("The sum of %d and %d is %ld\n", a, b, sum);
+//}
