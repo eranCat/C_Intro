fd478220c4d98279b2a571c6d7bb045382e42904
Karaso Eran <karasoe@my.hit.ac.il>
2022-10-05 18:45:52 +0300

    Fibonacci recursive

---
+++ C_Intro/FibonacciRec.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int Fibonacci(int n) {
+	if (n <= 1) return n;
+
+	return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+void main() {
+	printf("%d\n", Fibonacci(0));
+	printf("%d\n", Fibonacci(1));
+	printf("%d\n", Fibonacci(4));
+	printf("%d\n", Fibonacci(10));
+}
--- C_Intro/sumPowersRecursive.c
+++ C_Intro/sumPowersRecursive.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
-#include<math.h>
-
-double sum_powers_rec_1(double a, int n) {
-	if (n == 1) return a;
-	int pp = pow(a, n);
-	return sum_powers_rec_1(a, n - 1) + pp;
-}
-
-void main() {
-	printf("%.3lf", sum_powers_rec_1(2, 6));
-}
+//#include<stdio.h>
+//#include<math.h>
+//
+//double sum_powers_rec_1(double a, int n) {
+//	if (n == 1) return a;
+//	int pp = pow(a, n);
+//	return sum_powers_rec_1(a, n - 1) + pp;
+//}
+//
+//void main() {
+//	printf("%.3lf", sum_powers_rec_1(2, 6));
+//}
