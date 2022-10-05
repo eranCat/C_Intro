603ccfdb6d9729922eae22c7c0c36049c3e7e78c
Karaso Eran <karasoe@my.hit.ac.il>
2022-10-05 18:32:48 +0300

    Sum powers

--- C_Intro/FactorialRecursive.c
+++ C_Intro/FactorialRecursive.c
@@ -1,14 +1,14 @@
-#include<stdio.h>
-
-long factorial(int num);
-
-void main() {
-	long f = factorial(10);
-	printf("%d",f);
-}
-
-long factorial(int num) {
-	if (num <= 1) return 1;
-
-	return num * factorial(num - 1);
-}
+//#include<stdio.h>
+//
+//long factorial(int num);
+//
+//void main() {
+//	long f = factorial(10);
+//	printf("%d",f);
+//}
+//
+//long factorial(int num) {
+//	if (num <= 1) return 1;
+//
+//	return num * factorial(num - 1);
+//}
