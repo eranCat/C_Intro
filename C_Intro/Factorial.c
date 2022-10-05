34bd08dd5b5e9f2719516be863b96ea8984bbc59
Karaso Eran <karasoe@my.hit.ac.il>
2022-10-05 08:17:35 +0300

    while grades- example for do while input validation.

--- C_Intro/Factorial.c
+++ C_Intro/Factorial.c
@@ -1,12 +1,23 @@
 #include<stdio.h>
 
 /*The program calculates the factorial of 10 (10!)*/
-void main()
-{
-	int i = 2, result = 1;
-	while (i <= 10)
-	{
-		result *= i++;
-	}
-	printf("The result is % d", result);
-}
+//void main()
+//{
+//	int i = 2, result = 1;
+//	while (i <= 10)
+//	{
+//		result *= i++;
+//	}
+//	printf("The result is % d", result);
+//}
+
+/*The program calculates 2^10*/
+//void main() {
+//	int i = 1, result = 1;
+//	while (i <= 10)
+//	{
+//		result *= 2;
+//		i++;
+//	}
+//	printf("The result is % d", result);
+//}
