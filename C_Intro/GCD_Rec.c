b95ab99bfcf6287b2ad8e356010741bca295993e
Karaso Eran <karasoe@my.hit.ac.il>
2022-10-05 19:04:41 +0300

    Reverse number printing recursively

--- C_Intro/GCD_Rec.c
+++ C_Intro/GCD_Rec.c
@@ -1,14 +1,14 @@
-#include<stdio.h>
-
-int gcd(int a, int b) {
-	if (b == 0) return a;
-	return gcd(b, a % b);
-}
-
-void main() {
-	int gcdNum = gcd(270,100);
-	printf("%d\n", gcdNum);
-	
-	gcdNum = gcd(45, 345);
-	printf("%d\n", gcdNum);
-}
+//#include<stdio.h>
+//
+//int gcd(int a, int b) {
+//	if (b == 0) return a;
+//	return gcd(b, a % b);
+//}
+//
+//void main() {
+//	int gcdNum = gcd(270,100);
+//	printf("%d\n", gcdNum);
+//	
+//	gcdNum = gcd(45, 345);
+//	printf("%d\n", gcdNum);
+//}
---
+++ C_Intro/ReverseNumRec.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void reverseNum(int n) {
+	if (n == 0) 
+		return;
+
+	printf("%d", n%10);
+	reverseNum(n / 10);
+}
+
+void main() {
+	int num = 654321;
+	reverseNum(num);
+}
