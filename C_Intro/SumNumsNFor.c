900f6fe763cd28f9cede1b44976883e97c281743
Karaso Eran <karasoe@my.hit.ac.il>
2022-10-05 08:43:13 +0300

    sum of digits - for\while loops.

--- C_Intro/SumNumsNFor.c
+++ C_Intro/SumNumsNFor.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
-
-/*The program calculates the sum of n numbers the it will get from the user*/
-void main() {
-	int num, sum = 0, i, length;
-	printf("enter length of sequence\n");
-	scanf_s("%d", &length);
-	printf("enter % d integers \n", length);
-	for (i = 1; i <= length; i++)
-	{
-		scanf_s("%d", &num);
-		sum += num;
-	}
-	printf("the sum is % d\n", sum);
-}
+//#include<stdio.h>
+//
+///*The program calculates the sum of n numbers the it will get from the user*/
+//void main() {
+//	int num, sum = 0, i, length;
+//	printf("enter length of sequence\n");
+//	scanf_s("%d", &length);
+//	printf("enter % d integers \n", length);
+//	for (i = 1; i <= length; i++)
+//	{
+//		scanf_s("%d", &num);
+//		sum += num;
+//	}
+//	printf("the sum is % d\n", sum);
+//}
