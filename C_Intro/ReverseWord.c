39ff6507b89332fefb506dc4bacb02cb56b56586
Karaso Eran <karasoe@my.hit.ac.il>
2022-10-09 10:14:52 +0300

    Swap and max min between 2 numbers.

--- C_Intro/ReverseWord.c
+++ C_Intro/ReverseWord.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
-
-void reverese() {
-	char sc;//scanned 
-	sc = getchar();//scanf("%c", &sc);
-
-	if (sc != '*') {
-		reverese(sc);
-		putchar(sc);//printf("%c",sc);
-	}
-}
-
-void main() {
-	reverese();
-}
+//#include<stdio.h>
+//
+//void reverese() {
+//	char sc;//scanned 
+//	sc = getchar();//scanf("%c", &sc);
+//
+//	if (sc != '*') {
+//		reverese(sc);
+//		putchar(sc);//printf("%c",sc);
+//	}
+//}
+//
+//void main() {
+//	reverese();
+//}
